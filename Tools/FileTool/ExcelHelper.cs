4cc97f630cfd6ad798cb41130db0daa12abe7669
DickKuo <dick22707@gmail.com>
2015-02-26 09:36:56 +0800

    工具集
    CommTool功能內，加入Excel讀取資料功能。 #31

---
+++ Tools/FileTool/ExcelHelper.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.OleDb;
+using System.IO;
+
+namespace CommTool
+{
+    public class ExcelHelper
+    {
+
+
+        public DataTable ReadXlsTable(string FileName, int SheetNumber)
+        {
+
+        }
+
+
+        private bool isHDR = true;
+        public bool IsHDR
+        {
+            get
+            {
+                return isHDR;
+            }
+            set
+            {
+                isHDR = value;
+            }
+        }
+
+
+        /// <summary>
+        /// 20150130 Excel連線字串
+        /// </summary>
+        /// <param name="pExcelFilePath"></param>
+        /// <param name="isAdditional"></param>
+        /// <returns></returns>
+        private string GetConnectString(string pExcelFilePath, bool isAdditional)
+        {
+            string connStr = string.Empty;
+            bool isOccupied = false;
+            string msg = string.Empty;
+            bool IsInstalled = true;
+
+            connStr = "Provider = Microsoft.ACE.OLEDB.12.0 ; Data Source =" + pExcelFilePath + string.Format(";Extended Properties=\"Excel 12.0;HDR={0};IMEX=1\";", IsHDR ? "YES" : "No");
+            try
+            {
+                using (OleDbConnection con = new OleDbConnection(connStr))
+                {
+                    con.Open();
+                    con.Close();
+                    return connStr;
+                }
+            }
+            catch (Exception ex)
+            {
+                if (ex is OleDbException)
+                {
+                    isOccupied = true;
+                    msg += ex.Message + "\n";
+                }
+                else
+                {
+                    IsInstalled = false;
+                }
+            }
+            return connStr;
+        }
+
+        public List<string> GetSheetsNameByOpenXml(string filePath)
+        {
+            List<string> sheetNames = new List<string>();
+            FileStream pStream = File.OpenRead(filePath);
+            using (SpreadsheetDocument doc = SpreadsheetDocument.Open(pStream, false))
+            {
+                IEnumerable<Sheet> allSheets = doc.WorkbookPart.Workbook.Descendants<Sheet>();
+                foreach (Sheet itemSheet in allSheets)
+                {
+                    sheetNames.Add(itemSheet.Name.Value);
+                }
+            }
+            if (sheetNames.Count == 0)
+            {
+                MessageBox.Show("Excel里面没有任何页签!");
+            }
+            pStream.Close();
+            pStream.Dispose();
+            return sheetNames;
+        }
+
+
+    }
+}
