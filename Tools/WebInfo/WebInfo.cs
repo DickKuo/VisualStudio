a7dc67ae29f8a23aac1ed915f891d8bc1bc8d61a
Dick <erpbank.dick@gmail.com>
2015-04-08 21:10:42 +0800

    工具集
    加入讀取網頁轉字串功能。 #39

--- Tools/WebInfo/WebInfo.cs
+++ Tools/WebInfo/WebInfo.cs
@@ -25,7 +25,29 @@ namespace WebInfo
             return reader;
         }
 
-
+        /// <summary>
+        /// 抓取網頁資訊
+        /// </summary>
+        /// <param name="pUrl">網址</param>
+        /// <returns></returns>
+        public virtual string GetStringResponse(string pUrl)
+        {
+            string result = string.Empty;
+            try
+            {
+                WebRequest myWebRequest = WebRequest.Create(pUrl);
+                myWebRequest.Credentials = CredentialCache.DefaultCredentials;
+                HttpWebResponse response = (HttpWebResponse)myWebRequest.GetResponse();
+                Stream DataStream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(DataStream, Encoding.Default);
+                result = reader.ReadToEnd();
+            }
+            catch (Exception ex)
+            {
+                result = ex.Message;
+            }
+            return result;
+        }
 
     }
 }
