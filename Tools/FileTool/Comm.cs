63f56dafd989dcac653d4a9cb8cfd138e9f014fd
DickKuo <dick22707@gmail.com>
2015-04-22 16:56:37 +0800

    工具集
    #42

--- Tools/FileTool/Comm.cs
+++ Tools/FileTool/Comm.cs
@@ -70,44 +70,104 @@ namespace CommTool
     {
         private const string DefaulrLanguage = "1";
 
-        private static IServiceProvider _curentprovider;
+        //private static IServiceProvider _curentprovider;
 
-        public IServiceProvider curentprovider { get { return _curentprovider; } }
+        //public IServiceProvider curentprovider { get { return _curentprovider; } }
 
 
 
-        public static object GetSevice(Type ServiceType)
+        //public static object GetSevice(Type ServiceType)
+        //{
+        //    if (ServiceType == null)
+        //    {
+        //        throw new System.ArgumentNullException("ServiceType");
+        //    }
+        //    if (Initiator._curentprovider == null)
+        //    {
+        //        throw new ArgumentNullException(Resource.ProviderIsNull);
+        //    }
+        //    object service = Initiator._curentprovider.GetService(ServiceType);
+        //    if (service == null)
+        //    {
+        //        throw new DesignException(string.Format(Resources.FailGetService, serviceType.FullName));
+        //    }
+        //    return service;
+        //}
+
+        //public static T  GetSevice<T>(object ServiceName)        
+        //{
+        //    if (Initiator._curentprovider == null)
+        //    {
+        //        throw new ArgumentNullException(Resource.ProviderIsNull);
+        //    }
+        //    object service = Initiator._curentprovider.GetService(typeof(T));
+        //    if (service == null)
+        //    {
+        //        throw new ArgumentNullException(string.Format(Resources.FailGetService, typeof(T).FullName));
+        //    }
+        //    return (T)((object)service);
+        //}
+
+
+        /// <summary>
+        /// 20150108 add by Dick for 取得固定長度的字串 單位為Byte
+        /// </summary>
+        /// <param name="pString">輸入字串</param>
+        /// <param name="pLen">取值長度</param>
+        /// <param name="IsLeft">預設true從左算起;false為從右算起</param>
+        /// <param name="AddChar">字串長度不足時，補足字元</param>
+        /// <returns></returns>
+        private static string GetLenString(string pString, int pLen, bool IsLeft = true, char AddChar = ' ')
         {
-            if (ServiceType == null)
+            if (IsLeft)
             {
-                throw new System.ArgumentNullException("ServiceType");
+                pString = pString.PadRight(pLen, AddChar);
             }
-            if (Initiator._curentprovider == null)
+            else
             {
-                throw new ArgumentNullException(Resource.ProviderIsNull);
+                pString = pString.PadLeft(pLen, AddChar);
             }
-            object service = Initiator._curentprovider.GetService(ServiceType);
-            if (service == null)
+            string hopestring = string.Empty;  //希望取得的字串
+            string spp = string.Empty;
+            int byteLength;
+            if (IsLeft)
             {
-                throw new DesignException(string.Format(Resources.FailGetService, serviceType.FullName));
+                for (int i = 0; i <= pString.Length; i++)
+                {
+                    spp = pString.Substring(0, i);
+                    byteLength = System.Text.Encoding.Default.GetBytes(spp).Length;
+                    if (byteLength <= pLen)
+                    { hopestring = spp; }
+                    else
+                    {
+                        hopestring = pString.Substring(0, i - 1);
+                        break;
+                    }
+                }
             }
-            return service;
+            else
+            {
+                int getcount = 1;
+                for (int i = pString.Length - 1; i >= 0; i--)
+                {
+                    spp = pString.Substring(i, getcount);
+                    byteLength = System.Text.Encoding.Default.GetBytes(spp).Length;
+                    if (byteLength <= pLen)
+                    { hopestring = spp; }
+                    else
+                    {
+                        hopestring = pString.Substring(i, getcount);
+                        break;
+                    }
+                    if (byteLength == pLen)
+                    {
+                        break;
+                    }
+                    getcount++;
+                }
+            }
+            return hopestring;
         }
-
-        public static T  GetSevice<T>(object ServiceName)        
-        {
-            if (Initiator._curentprovider == null)
-            {
-                throw new ArgumentNullException(Resource.ProviderIsNull);
-            }
-            object service = Initiator._curentprovider.GetService(typeof(T));
-            if (service == null)
-            {
-                throw new ArgumentNullException(string.Format(Resources.FailGetService, typeof(T).FullName));
-            }
-            return (T)((object)service);
-        }
-
 
     }
 }
