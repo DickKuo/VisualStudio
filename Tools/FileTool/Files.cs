87cdec613e79c0d41361963902c44ae4b0c8afa0
DickKuo <dick22707@gmail.com>
2015-04-28 13:48:28 +0800

    工具集
    更新DLL#43

--- Tools/FileTool/Files.cs
+++ Tools/FileTool/Files.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using System.Xml;
+using System.Diagnostics;
 
 namespace CommTool
 {
@@ -96,5 +98,84 @@ namespace CommTool
                 ToolLog.Log(ex);
             }
         }
+
+
+
+        /// <summary>
+        /// 20141219 add by Dick for 更新檔案
+        /// </summary>
+        /// <param name="UpdatePath"></param>
+        public virtual void UpdateDll(string UpdatePath)
+        {
+            try
+            {
+                DirectoryInfo LocalDir = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory);
+                List<string> LocalFileList = new List<string>();
+                foreach (FileInfo FiInfo in LocalDir.GetFiles())
+                {
+                    LocalFileList.Add(FiInfo.Name);
+                }
+                DirectoryInfo RemoveDir = new DirectoryInfo(UpdatePath);
+                foreach (FileInfo FiInfo in RemoveDir.GetFiles())
+                {
+                    if (FiInfo.Name.Equals("FileTool.dll"))
+                    {
+                        continue;
+                    }
+                    if (!LocalFileList.Contains(FiInfo.Name))
+                    {
+                        File.Copy(FiInfo.FullName, AppDomain.CurrentDomain.BaseDirectory + Path.DirectorySeparatorChar + FiInfo.Name, true);
+                    }
+                    else
+                    {
+                        if (FiInfo.Extension.Equals(".xml"))
+                        {
+                            try
+                            {
+                                XmlDocument SourceDoc = XmlFile.LoadXml(FiInfo.FullName);
+                                XmlNode root = SourceDoc.SelectSingleNode("root");
+                                XmlDocument DeInfo = XmlFile.LoadXml(AppDomain.CurrentDomain.BaseDirectory + Path.DirectorySeparatorChar + FiInfo.Name);
+                                XmlNode deroot = DeInfo.SelectSingleNode("root");
+                                if (root != null && deroot != null)
+                                {
+                                    if (!root.Attributes["Version"].Value.Equals(deroot.Attributes["Version"].Value))
+                                    {
+                                        try
+                                        {
+                                            File.Copy(FiInfo.FullName, AppDomain.CurrentDomain.BaseDirectory + Path.DirectorySeparatorChar + FiInfo.Name, true);
+                                        }
+                                        catch (Exception ex)
+                                        {
+                                            Console.WriteLine(ex.Message);
+                                        }
+                                    }
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine(ex.Message);
+                            }
+                        }
+                        else
+                        {
+                            FileVersionInfo SourceFile = FileVersionInfo.GetVersionInfo(FiInfo.FullName); //遠端檔案
+                            FileVersionInfo Info = FileVersionInfo.GetVersionInfo(AppDomain.CurrentDomain.BaseDirectory + Path.DirectorySeparatorChar + FiInfo.Name); //本地檔案
+                            if (SourceFile.FileVersion != null)
+                            {
+                                if (!SourceFile.FileVersion.Equals(Info.FileVersion))
+                                {
+                                    File.Copy(SourceFile.FileName, Info.FileName, true);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+               ToolLog.Log(ex.Message);
+            }
+        }
+
     }
 }
