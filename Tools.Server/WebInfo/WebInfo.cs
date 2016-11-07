ff13b529ed3d8ff77d4e62a10bb4bc76b81b7bfd
DickKuo <dick22707@gmail.com>
2016-11-07 15:48:06 +0800

    工具集
    修改JSON格式 #41

--- Tools.Server/WebInfo/WebInfo.cs
+++ Tools.Server/WebInfo/WebInfo.cs
@@ -20,10 +20,15 @@ namespace WebInfo
             public const string Re = "Re: ";
             public const string PostError = "POST失敗：";
             public const int Zero = 0;
+            public const int MinItem = 0;
             public const int FirstItem = 0;
             public const string Post = "Post";
             public const string Get = "Get";
             public const string ContentType = "text/xml; encoding='utf-8'";
+            public const string UTF8 = "utf-8";
+            public const string PostMessage = "Post Start";
+            public const string JsonKey = "json";
+            public const string JSonStringFormat = "{0}={1}";
         }
 
         private string _pLogPath;
@@ -60,14 +65,27 @@ namespace WebInfo
         /// <param name="pUrl">網址</param>
         /// <returns></returns>
         public StreamReader GetResponse(string pUrl) {
-            WebRequest myWebRequest = WebRequest.Create(pUrl);
+            WebRequest myWebRequest = WebRequest.Create( pUrl);
             myWebRequest.Credentials = CredentialCache.DefaultCredentials;
             HttpWebResponse response = (HttpWebResponse)myWebRequest.GetResponse();
             Stream DataStream = response.GetResponseStream();
             StreamReader reader = new StreamReader(DataStream, Encoding.Default);
             return reader;
         }
-
+        
+        /// <summary>抓取網頁資訊</summary>
+        /// <param name="pUrl"></param>
+        /// <param name="Code"></param>
+        /// <returns></returns>
+        public StreamReader GetResponse(string pUrl,Encoding Coding) { 
+            WebRequest myWebRequest = WebRequest.Create(pUrl);
+            myWebRequest.Credentials = CredentialCache.DefaultCredentials;
+            HttpWebResponse response = (HttpWebResponse)myWebRequest.GetResponse();
+            Stream DataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(DataStream, Coding);
+            return reader;
+        }
+              
         /// <summary> 抓取網頁資訊To String </summary>
         /// <param name="pUrl">網址</param>
         /// <returns></returns>
@@ -82,7 +100,23 @@ namespace WebInfo
             }
             return result;
         }
-
+        
+        /// <summary>抓取網頁資訊To String  </summary>
+        /// <param name="pUrl"></param>
+        /// <param name="Code"></param>
+        /// <returns></returns>
+        public virtual string GetStringResponse(string pUrl, Encoding Code) {
+            string result = string.Empty;
+            try {
+                StreamReader reader = GetResponse(pUrl, Code);
+                result = reader.ReadToEnd();
+            }
+            catch (Exception ex) {
+                result = ex.Message;
+            }
+            return result;
+        }
+        
         /// <summary>Post功能</summary>
         /// 20160909 加入紀錄POST的JSON資料 By Dick 
         /// <param name="Address">網址</param>
@@ -95,11 +129,15 @@ namespace WebInfo
                 info = SiteInfoList[Default.FirstItem];
             }
             try {
-                ToolLog.Log("Post Start" + info.Title);
-                string strJson = "SiteInfoList=" + JsonConvert.SerializeObject(SiteInfoList, Newtonsoft.Json.Formatting.Indented);
+                ToolLog.Log(Default.PostMessage + info.Title);
+                PostData Data = new PostData();
+                Data.Author = SiteInfoList[Default.MinItem].Author;
+                Data.Content = SiteInfoList[Default.MinItem].Context;
+                Data.Title = SiteInfoList[Default.MinItem].Title;
+                string strJson = string.Format(Default.JSonStringFormat, Default.JsonKey, JsonConvert.SerializeObject(Data, Newtonsoft.Json.Formatting.Indented));
                 HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Address);
                 request.ContentType = "application/x-www-form-urlencoded";
-                request.Method = Default.Post;
+                request.Method = Default.Post;  
                 byte[] byteArray = Encoding.UTF8.GetBytes(strJson);
                 request.ContentLength = byteArray.Length;
                 using (Stream dataStream = request.GetRequestStream()) {
@@ -107,7 +145,7 @@ namespace WebInfo
                 }
                 using (HttpWebResponse response = (HttpWebResponse)request.GetResponse()) {
                     Stream receiveStream = response.GetResponseStream();
-                    Encoding encode = System.Text.Encoding.GetEncoding("utf-8");
+                    Encoding encode = System.Text.Encoding.GetEncoding(Default.UTF8);
                     StreamReader readStream = new StreamReader(receiveStream, encode);
                     length = response.ContentLength;
                     string st = readStream.ReadToEnd();
@@ -123,7 +161,30 @@ namespace WebInfo
             }
             return length;
         }
-         
+
+        /// <summary>用GET方式PO文</summary>
+        /// <param name="Address"></param>
+        /// <param name="SiteInfoList"></param>
+        public void BueatyPostContentByGetMothed(string Address, List<SiteInfo> SiteInfoList) {
+            SiteInfo info = new SiteInfo();
+            if (SiteInfoList.Count > Default.Zero) {
+                info = SiteInfoList[Default.FirstItem];
+            }
+            try {
+                ToolLog.Log(Default.PostMessage + info.Title);
+                PostData Data = new PostData();
+                Data.Author = SiteInfoList[Default.MinItem].Author;
+                Data.Content = SiteInfoList[Default.MinItem].Context;
+                Data.Title = SiteInfoList[Default.MinItem].Title;
+                string strJson = string.Format(Default.JSonStringFormat, Default.JsonKey, JsonConvert.SerializeObject(Data, Newtonsoft.Json.Formatting.Indented));
+                string FullPath = Address + strJson;
+                GetStringResponse(FullPath);        
+                ToolLog.Record(strJson);
+            }
+            catch (Exception ex) {
+                ToolLog.Log(CommTool.LogType.Error, Default.PostError + ex.Message);  
+            }
+        }
 
         /// <summary>直接針對網址進行解析內容及傳輸</summary>
         /// 20150410 add by Dick for 直接針對網址進行解析內容及傳輸。
@@ -171,7 +232,15 @@ namespace WebInfo
                     }
                 }
             }
-        }
-
+        }       
     }
+}
+
+/// <summary>輔助Class</summary>
+public class PostData {
+    public string Author { set; get; }
+
+    public string Content { set; get; }
+
+    public string Title { set; get; }
 }
