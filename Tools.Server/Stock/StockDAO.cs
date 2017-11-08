955a38d4835199fa3c4052497bb2a75b654b47c4
DickKuo <dick22707@gmail.com>
2017-11-08 13:52:48 +0800

    每月結算
    修正月選結算周不會有指標的問題
    #114

--- Tools.Server/Stock/StockDAO.cs
+++ Tools.Server/Stock/StockDAO.cs
@@ -1021,8 +1021,8 @@ namespace Stock {
                                     dt = USP.ExeProcedureGetDataTable(SP.GetMaxVolume);
                                 }
                                 else {
-                                    USP.AddParameter(SPParameter.OP, OP);
-                                    USP.AddParameter(SPParameter.DueMonth, _Calendar.NearMonth1);
+                                    //USP.AddParameter(SPParameter.OP, OP);
+                                    //USP.AddParameter(SPParameter.DueMonth, _Calendar.NearMonth1);
                                     dt = USP.ExeProcedureGetDataTable(SP.GetMaxNumberOfContracts);
                                     #region 最大未平昌點數過小時，就使用最大交易量作為基準
                                     if (dt != null && dt.Rows.Count > 0)
@@ -1033,6 +1033,11 @@ namespace Stock {
                                             USP.AddParameter(SPParameter.DueMonth, _Calendar.NearMonth1);
                                             dt = USP.ExeProcedureGetDataTable(SP.GetMaxVolume);
                                         }
+                                    }
+                                    else {
+                                        USP.AddParameter(SPParameter.OP, OP);
+                                        USP.AddParameter(SPParameter.DueMonth, _Calendar.NearMonth1);
+                                        dt = USP.ExeProcedureGetDataTable(SP.GetMaxVolume);
                                     }
                                     #endregion
                                 }
